814b4785f939d05d45e7bb77b37cd1abd9bd4d1a
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-03-24 07:40:13 -0300

    ✨ add initial component structure and styles for home, about, skills, contact, projects, footer, header, skill-card, stats-card, contact-form, project-card, work-experience, contact-info-card, and work-experience-card

---
+++ src/app/components/contact-info-card/contact-info-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-info-card',
+  imports: [],
+  templateUrl: './contact-info-card.component.html',
+  styleUrl: './contact-info-card.component.css'
+})
+export class ContactInfoCardComponent {
+
+}
---
+++ src/app/components/work-experience-card/work-experience-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience-card',
+  imports: [],
+  templateUrl: './work-experience-card.component.html',
+  styleUrl: './work-experience-card.component.css'
+})
+export class WorkExperienceCardComponent {
+
+}
