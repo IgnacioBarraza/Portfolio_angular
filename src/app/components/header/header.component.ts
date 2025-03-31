523ba2698478bd4750f9001385911d687f3965a7
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2025-03-30 23:28:18 -0300

    ✨ add favicon, background image, Tailwind CSS configuration, and update home component structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  navItems: NavItem[] = [
+    { label: "About", japaneseLabel: "私", href: "#about" },
+    { label: "Skills", japaneseLabel: "技", href: "#skills" },
+    { label: "Experience", japaneseLabel: "経", href: "#experience" },
+    { label: "Projects", japaneseLabel: "作", href: "#projects" },
+    { label: "Contact", japaneseLabel: "連", href: "#contact" },
+  ]
 }
---
+++ src/app/utils/interfaces.ts
@@ -0,0 +1,5 @@
+interface NavItem {
+  label: string
+  japaneseLabel: string
+  href: string
+}
